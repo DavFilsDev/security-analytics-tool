bfed3c3d80f93a58fe25e2a161e0a4396c978f3d
DavFilsDev <miharisoadavidfils@gmail.com>
2026-02-27 23:09:20 +0300

    feat(core): initialize core module with package exports

---
+++ src/core/__init__.py
@@ -0,0 +1,4 @@
+from .operations import add, subtract, multiply, divide
+from .exceptions import CalculationError, DivisionByZeroError
+
+__all__ = ['add', 'subtract', 'multiply', 'divide', 'CalculationError', 'DivisionByZeroError']
